3bb3d72f89134694b9901f46a48dd4114abd08d7
アクゼスティア <akzestia@gmail.com>
2026-06-17 14:22:54 +0900

    Fixed some stuff ^_^

--- lua/アクゼスティア/plugins/blink.lua
+++ lua/アクゼスティア/plugins/blink.lua
@@ -1,52 +1,52 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-  version = '1.*',
+	version = "1.*",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { 
-        preset = 'default',
-        ["<CR>"] = { "accept", "fallback" },
-        ["<S-j>"] = { "select_next", "fallback" },
-        ["<S-k>"] = { "select_prev", "fallback" },
-    },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "accept", "fallback" },
+			["<D-j>"] = { "select_next", "fallback" },
+			["<D-k>"] = { "select_prev", "fallback" },
+		},
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = {
-      menu = {
-        border = "rounded",
-        draw = {
-          treesitter = { "lsp" },
-          columns = {
-            { "kind_icon", "label", gap = 1 },
-            { "kind" },
-          },
-        },
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
-        window = { border = "rounded" },
-      },
-      ghost_text = { enabled = true },
-    },
+		completion = {
+			menu = {
+				border = "rounded",
+				draw = {
+					treesitter = { "lsp" },
+					columns = {
+						{ "kind_icon", "label", gap = 1 },
+						{ "kind" },
+					},
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = { border = "rounded" },
+			},
+			ghost_text = { enabled = true },
+		},
 
-    signature = {
-      enabled = true,
-      window = { border = "rounded" },
-    },
+		signature = {
+			enabled = true,
+			window = { border = "rounded" },
+		},
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
--- lua/アクゼスティア/plugins/telescope.lua
+++ lua/アクゼスティア/plugins/telescope.lua
@@ -22,6 +22,18 @@ return {
 					"vendor/",
 					"%.lock",
 				},
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob",
+					"!**/.git/*",
+				},
 				mappings = {
 					i = {
 						["<S-j>"] = actions.move_selection_next,
@@ -31,6 +43,35 @@ return {
 						["<S-j>"] = actions.move_selection_next,
 						["<S-k>"] = actions.move_selection_previous,
 					},
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = {
+						"fd",
+						"--type",
+						"f",
+						"--hidden",
+						"--strip-cwd-prefix",
+						"--exclude",
+						".git",
+						"--exclude",
+						"node_modules",
+						"--exclude",
+						"external",
+						"--exclude",
+						"build",
+						"--exclude",
+						"target",
+					},
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
 				},
 			},
 		})
